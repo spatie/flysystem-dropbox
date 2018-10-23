41fbda47325d2c1cce8f3c0cd3769d2e308fb590
Isaac Ferreira <zaclummys@gmail.com>
2018-10-23 15:10:43 -0300

    Update DropboxAdapter.php
    Add support to `temporaryUrl()`.

--- src/DropboxAdapter.php
+++ src/DropboxAdapter.php
@@ -244,6 +244,11 @@ class DropboxAdapter extends AbstractAdapter
     {
         return $this->client->getTemporaryLink($path);
     }
+    
+    public function getTemporaryUrl(string $path): string
+    {
+        return $this->getTemporaryLink($path);
+    }
 
     public function getThumbnail(string $path, string $format = 'jpeg', string $size = 'w64h64')
     {
